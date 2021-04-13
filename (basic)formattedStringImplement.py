dc0bbc82d0ad4d615d6576b5e9424d7b8e924f46
SalomoMandagi11 <81841795+SalomoMandagi11@users.noreply.github.com>
2021-04-13 08:59:48 +0800

    Add files via upload

---
+++ (basic)formattedStringImplement.py
@@ -0,0 +1,15 @@
+"""
+    Disini kita akan mengimplementasikan formatted string atau dalam
+    codenya itu seperti ini: f"{nama_string}"
+
+    Apa fungsinya?
+    Fungsinya ya pastinya berhubungan dengan string, lebih tepatnya supaya string terlihat
+    lebih rapi saja. (pengertian sementara) (11 April 2021)
+"""
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+work = input("What is your job? ")
+
+print(f"Your name is {name}, and you are {age} years old, work as {work}")
+
